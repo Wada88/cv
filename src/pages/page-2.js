2bfa58c91f2f3b2884039d74eba5cbe826156ca9
Wada88 <matti.wada@gmail.com>
2019-03-31 00:15:24 +0200

    ekat lisäykset

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
+import duomo from '../images/duomo.jpg'
 import Layout from '../components/layout'
 
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
+    <h1>MInä</h1>
     <p>Welcome to page 2</p>
+    <img src={duomo} alt="" className="duomo" />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
